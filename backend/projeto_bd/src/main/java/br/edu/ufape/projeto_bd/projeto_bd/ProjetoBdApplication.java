3d90b0b65883d7070bd02025f796f595f1014d02
caiogmrocha <caioguilherme739@gmail.com>
2025-08-21 21:18:18 -0300

    chore(backend): bootstrap Spring Framework API REST backend

---
+++ backend/projeto_bd/src/main/java/br/edu/ufape/projeto_bd/projeto_bd/ProjetoBdApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.projeto_bd.projeto_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBdApplication.class, args);
+	}
+
+}
---
+++ backend/projeto_bd/src/test/java/br/edu/ufape/projeto_bd/projeto_bd/ProjetoBdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.projeto_bd.projeto_bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBdApplicationTests {
+
+	// @Test
+	void contextLoads() {
+	}
+
+}
