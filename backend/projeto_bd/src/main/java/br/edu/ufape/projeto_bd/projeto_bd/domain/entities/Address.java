d03a84a6883cd5d3a85b300d044ada7151ce2277
caiogmrocha <caioguilherme739@gmail.com>
2025-08-21 23:13:25 -0300

    feat(backend,database): create Address entity

---
+++ backend/projeto_bd/src/main/java/br/edu/ufape/projeto_bd/projeto_bd/domain/entities/Address.java
@@ -0,0 +1,63 @@
+package br.edu.ufape.projeto_bd.projeto_bd.domain.entities;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLDelete(sql = "UPDATE addresses SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction(value = "SELECT * FROM addresses WHERE deleted_at IS NULL")
+public class Address {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Integer id;
+
+  @Column(name = "country", length = 50)
+  private String country;
+
+  @Column(name = "state", length = 50)
+  private String state;
+
+  @Column(name = "city", length = 50)
+  private String city;
+
+  @Column(name = "street", length = 100)
+  private String street;
+
+  @Column(name = "number", length = 10)
+  private String number;
+
+  @Column(name = "zip_code", length = 15)
+  private String zipCode;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+
+  @Column(name = "deleted_at", nullable = false)
+  private LocalDateTime deletedAt;
+}
