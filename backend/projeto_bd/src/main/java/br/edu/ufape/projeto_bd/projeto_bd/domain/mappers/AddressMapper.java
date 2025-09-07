191b2755ab56eb4106f101ac8ab61016399c70a7
Analice Silva <analiceds.nascimento@gmail.com>
2025-09-06 21:53:59 -0300

    [feat][backend] Adiciona mappers para conversão entre DTOs e entidades do supplier

---
+++ backend/projeto_bd/src/main/java/br/edu/ufape/projeto_bd/projeto_bd/domain/mappers/AddressMapper.java
@@ -0,0 +1,20 @@
+package br.edu.ufape.projeto_bd.projeto_bd.domain.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+import br.edu.ufape.projeto_bd.projeto_bd.domain.dtos.AddressDTO;
+import br.edu.ufape.projeto_bd.projeto_bd.domain.entities.Address;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface AddressMapper {
+    
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true) 
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    Address toEntity(AddressDTO dto);
+    
+    AddressDTO toDTO(Address entity);
+}
