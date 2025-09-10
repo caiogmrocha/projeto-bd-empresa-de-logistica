be62e5ea9feee6338bbc80a454f5925e57c4b6b4
caiogmrocha <caioguilherme739@gmail.com>
2025-09-10 01:10:26 -0300

    feat(db): add migration to seed languages (V23__seed_languages.sql)

---
+++ backend/projeto_bd/src/main/resources/db/migrations/V23__seed_languages.sql
@@ -0,0 +1,209 @@
+DELETE FROM languages;
+
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('Аҧсуа','ab'),
+	 ('Qafar af','aa'),
+	 ('Afrikaans','af'),
+	 ('Ákán','ak'),
+	 ('Shqip','sq'),
+	 ('አማርኛ (Amarəñña)','am'),
+	 ('اَلْعَرَبِيَّةُ
+(al-ʿarabiyyah)','ar'),
+	 ('Aragonés','an'),
+	 ('Հայերեն (Hayeren)','hy'),
+	 ('অসমীয়া (Ôxômiya)','as');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('Авар мацӏ','av'),
+	 ('Upastawakaēna','ae'),
+	 ('Aymara','ay'),
+	 ('Azərbaycan dili','az'),
+	 ('بَمَنَنكَن','bm'),
+	 ('Башҡорт теле','ba'),
+	 ('Euskara','eu'),
+	 ('Беларуская мова','be'),
+	 ('বাংলা (Bāŋlā)','bn'),
+	 ('Bislama','bi');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('Босански (Bosanski)','bs'),
+	 ('Brezhoneg','br'),
+	 ('Български (Bulgarski)','bg'),
+	 ('မြန်မာစာ (Mrãmācā)','my'),
+	 ('Català','ca'),
+	 ('Finu'' Chamoru','ch'),
+	 ('Нохчийн мотт','ce'),
+	 ('Chichewa','ny'),
+	 ('中文 (Zhōngwén)
+汉语','zh'),
+	 ('Славе́нскїй ѧ҆зы́къ','cu');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('Чӑвашла (Çăvaşla)','cv'),
+	 ('Kernowek','kw'),
+	 ('Corsu','co'),
+	 ('ᓀᐦᐃᔭᐁᐧᐃᐧᐣ (Nehiyawewin)','cr'),
+	 ('Hrvatski','hr'),
+	 ('Čeština','cs'),
+	 ('Dansk','da'),
+	 ('ދިވެހި (Dhivehi)','dv'),
+	 ('Nederlands','nl'),
+	 ('རྫོང་ཁ་ (Dzongkha)','dz');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('English','en'),
+	 ('Esperanto','eo'),
+	 ('Eesti keel','et'),
+	 ('Èʋegbe','ee'),
+	 ('Føroyskt','fo'),
+	 ('Na Vosa Vakaviti','fj'),
+	 ('Suomi','fi'),
+	 ('Français','fr'),
+	 ('Frysk','fy'),
+	 ('𞤊𞤵𞤤𞤬𞤵𞤤𞤣𞤫','ff');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('Gàidhlig','gd'),
+	 ('Galego','gl'),
+	 ('Luganda','lg'),
+	 ('ქართული (Kharthuli)','ka'),
+	 ('Deutsch','de'),
+	 ('Νέα Ελληνικά','el'),
+	 ('Kalaallisut','kl'),
+	 ('Avañe''ẽ','gn'),
+	 ('ગુજરાતી (Gujarātī)','gu'),
+	 ('Kreyòl ayisyen','ht');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('هَرْشٜن هَوْس (halshen Hausa)','ha'),
+	 ('עברית‎ (Ivrit)','he'),
+	 ('Otjiherero','hz'),
+	 ('हिन्दी (Hindī)','hi'),
+	 ('Hiri Motu','ho'),
+	 ('Magyar nyelv','hu'),
+	 ('Íslenska','is'),
+	 ('Ido','io'),
+	 ('ásụ̀sụ́ Ìgbò','ig'),
+	 ('bahasa Indonesia','id');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('Interlingua','ia'),
+	 ('Interlingue','ie'),
+	 ('ᐃᓄᒃᑎᑐᑦ (Inuktitut)','iu'),
+	 ('Iñupiaq','ik'),
+	 ('Gaeilge','ga'),
+	 ('Italiano','it'),
+	 ('日本語 (Nihongo)','ja'),
+	 ('ꦧꦱꦗꦮ','jv'),
+	 ('ಕನ್ನಡ (Kannađa)','kn'),
+	 ('كَنُرِيِه','kr');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('कॉशुर','ks'),
+	 ('Қазақша','kk'),
+	 ('ខេមរភាសា','km'),
+	 ('Gĩgĩkũyũ','ki'),
+	 ('Ikinyarwanda','rw'),
+	 ('Кыргыз','ky'),
+	 ('Коми кыв','kv'),
+	 ('Kikongo','kg'),
+	 ('한국어 (Hangugeo)
+조선말 (Chosŏnmal)','ko'),
+	 ('Oshikwanyama','kj');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('کوردی','ku'),
+	 ('ພາສາລາວ (phasa Lao)','lo'),
+	 ('Latinum','la'),
+	 ('Latviski','lv'),
+	 ('Lèmburgs','li'),
+	 ('Lingála','ln'),
+	 ('Lietuvių','lt'),
+	 ('Kiluba','lu'),
+	 ('Lëtzebuergesch','lb'),
+	 ('Македонски (Makedonski)','mk');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('مَلَغَسِ','mg'),
+	 ('بهاس ملايو (bahasa Melayu)','ms'),
+	 ('മലയാളം (Malayāļã)','ml'),
+	 ('Malti','mt'),
+	 ('Gaelg','gv'),
+	 ('reo Māori','mi'),
+	 ('मराठी (Marāṭhī)','mr'),
+	 ('kajin M̧ajel‌̧','mh'),
+	 ('ᠮᠣᠩᠭᠣᠯ
+ᠬᠡᠯᠡ','mn'),
+	 ('dorerin Naoe','na');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('Diné bizaad','nv'),
+	 ('isiNdebele','nd'),
+	 ('isiNdebele','nr'),
+	 ('Ndonga','ng'),
+	 ('नेपाली भाषा (Nepālī bhāśā)','ne'),
+	 ('Norsk','no'),
+	 ('Norsk Bokmål','nb'),
+	 ('Norsk Nynorsk','nn'),
+	 ('Occitan','oc'),
+	 ('ᐊᓂᔑᓈᐯᒧᐎᓐ','oj');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('ଓଡ଼ିଆ (Odia)','or'),
+	 ('afaan Oromoo','om'),
+	 ('ирон Ӕвзаг
+(iron Ævzag)','os'),
+	 ('Pāli','pi'),
+	 ('پښتو (Pax̌tow)','ps'),
+	 ('فارسی (Fārsiy)','fa'),
+	 ('Polski','pl'),
+	 ('Português','pt'),
+	 ('ਪੰਜਾਬੀ','pa'),
+	 ('Runa simi','qu');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('Română','ro'),
+	 ('Rumantsch','rm'),
+	 ('Ikirundi','rn'),
+	 ('Русский язык (Russkiĭ âzyk)','ru'),
+	 ('Davvisámegiella','se'),
+	 ('gagana Sāmoa','sm'),
+	 ('yângâ tî Sängö','sg'),
+	 ('संस्कृतम् (Saṃskṛtam)','sa'),
+	 ('Sardu','sc'),
+	 ('Српски (Srpski)','sr');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('chiShona','sn'),
+	 ('سنڌي','sd'),
+	 ('සිංහල (Siṁhala)','si'),
+	 ('Slovenčina','sk'),
+	 ('Slovenščina','sl'),
+	 ('Soomaali','so'),
+	 ('Sesotho','st'),
+	 ('Español','es'),
+	 ('basa Sunda','su'),
+	 ('Kiswahili','sw');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('siSwati','ss'),
+	 ('Svenska','sv'),
+	 ('Wikang Tagalog','tl'),
+	 ('reo Tahiti','ty'),
+	 ('Тоҷикӣ (Tojikī)','tg'),
+	 ('தமிழ் (Tamiḻ)','ta'),
+	 ('Татар теле','tt'),
+	 ('తెలుగు (Telugu)','te'),
+	 ('ภาษาไทย (Phasa Thai)','th'),
+	 ('བོད་སྐད་ (Bodskad)','bo');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('ትግርኛ (Təgrəñña)','ti'),
+	 ('lea faka-Tonga','to'),
+	 ('Xitsonga','ts'),
+	 ('Setswana','tn'),
+	 ('Türkçe','tr'),
+	 ('Türkmençe','tk'),
+	 ('Twi','tw'),
+	 ('ئۇيغۇر تىلى','ug'),
+	 ('Українська (Ukraїnska)','uk'),
+	 ('اُردُو (Urduw)','ur');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('Ózbekça','uz'),
+	 ('Tshivenḓa','ve'),
+	 ('tiếng Việt','vi'),
+	 ('Volapük','vo'),
+	 ('Walon','wa'),
+	 ('Cymraeg','cy'),
+	 ('وࣷلࣷفْ','wo'),
+	 ('isiXhosa','xh'),
+	 ('ꆈꌠꉙ (Nuosuhxop)','ii'),
+	 ('ייִדיש (Yidiš)','yi');
+INSERT INTO languages (language_name,iso_code) VALUES
+	 ('èdè Yorùbá','yo'),
+	 ('話僮 (Vahcuengh)','za'),
+	 ('isiZulu','zu');
