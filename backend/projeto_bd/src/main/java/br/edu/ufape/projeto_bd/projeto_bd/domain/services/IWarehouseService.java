2553678731746b78f3019653a3bac51e23fadcc8
Bruna Ferreira <brunagaby8@gmail.com>
2025-09-12 18:32:49 -0300

    [feat][backend] Adiciona a implementação da classe WarehouseService e a interface IWarehouseService com métodos CRUD

---
+++ backend/projeto_bd/src/main/java/br/edu/ufape/projeto_bd/projeto_bd/domain/services/IWarehouseService.java
@@ -0,0 +1,19 @@
+package br.edu.ufape.projeto_bd.projeto_bd.domain.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import br.edu.ufape.projeto_bd.projeto_bd.domain.dtos.RequestDTO.WarehouseRequestDTO;
+import br.edu.ufape.projeto_bd.projeto_bd.domain.dtos.ResponseDTO.WarehouseResponseDTO;
+
+public interface IWarehouseService {
+    public WarehouseResponseDTO createWarehouse (WarehouseRequestDTO request);
+
+    public WarehouseResponseDTO findWarehouseById(Long id);
+
+    public Page<WarehouseResponseDTO> findAllWarehouses(Pageable pageable);
+
+    public WarehouseResponseDTO updateWarehouse(Long id, WarehouseRequestDTO request);
+
+    public void deleteWarehouse(Long id);
+
+}
